20f213cb51cc12fb0b394c741af3c80c04c9c5a9
ROHITHKATARI <146699040+ROHITHKATARI@users.noreply.github.com>
2026-02-08 18:05:07 +0530

    Update app.js
    Updated App.js

--- beanstalk/app.js
+++ beanstalk/app.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 3000,
+var port = process.env.PORT || 8080,
     http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('index.html');
@@ -18,7 +18,7 @@ var server = http.createServer(function (req, res) {
         req.on('end', function() {
             if (req.url === '/') {
                 log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
+            } else if (req.url === '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
 
